22117f0c26305019356287333cfdca8a1dd80661
phantom <non3gov@gmail.com>
2017-01-03 15:48:55 +0800

    稍微修改

--- milogin.py
+++ milogin.py
@@ -13,6 +13,7 @@ import re
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
 def getToken(host):
 
     homeRequest = requests.get('http://' + host + '/cgi-bin/luci/web/home')
@@ -20,9 +21,7 @@ def getToken(host):
     mac = re.findall(r'deviceId = \'(.*)\';', homeRequest.text)[0]
 
     aimurl = "http://" + host + "/cgi-bin/luci/api/xqsystem/login"
-
     nonce = "0_" + mac + "_" + str(int(time.time())) + "_" + str(random.randint(1000, 10000))
-
     pwdtext = sys.argv[2]
 
     pwd = SHA.new()
@@ -51,12 +50,12 @@ def getToken(host):
 
 def getInfo(host, token):
 
-    base_url = 'http://'+ host + '/cgi-bin/luci/;stok='+ token + '/api/misystem/status'
-    wan_url = 'http://'+ host + '/cgi-bin/luci/;stok='+ token + '/api/xqnetwork/wan_info'
-    stop_url = 'http://'+ host + '/cgi-bin/luci/;stok='+ token + '/api/xqnetwork/pppoe_stop'
-    start_url = 'http://'+ host + '/cgi-bin/luci/;stok='+ token + '/api/xqnetwork/pppoe_start'
-    pppoe_url = 'http://'+ host + '/cgi-bin/luci/;stok='+ token + '/api/xqnetwork/pppoe_status'
-    reboot_url = 'http://'+ host + '/cgi-bin/luci/;stok='+ token + '/api/xqnetwork/reboot?client=web'
+    base_url = 'http://' + host + '/cgi-bin/luci/;stok=' + token + '/api/misystem/status'
+    wan_url = 'http://' + host + '/cgi-bin/luci/;stok=' + token + '/api/xqnetwork/wan_info'
+    stop_url = 'http://' + host + '/cgi-bin/luci/;stok=' + token + '/api/xqnetwork/pppoe_stop'
+    start_url = 'http://' + host + '/cgi-bin/luci/;stok=' + token + '/api/xqnetwork/pppoe_start'
+    pppoe_url = 'http://' + host + '/cgi-bin/luci/;stok=' + token + '/api/xqnetwork/pppoe_status'
+    reboot_url = 'http://' + host + '/cgi-bin/luci/;stok=' + token + '/api/xqnetwork/reboot?client=web'
 
     getStatus(base_url)
     getWanInfo(wan_url)
@@ -64,7 +63,7 @@ def getInfo(host, token):
     action = sys.argv[3]
 
     if action == 'reconnect':
-        doReconnect(stop_url,start_url,pppoe_url)
+        doReconnect(stop_url, start_url, pppoe_url)
     elif action == 'restart':
         doReboot(reboot_url)
     else:
@@ -74,15 +73,14 @@ def getInfo(host, token):
 def getStatus(url):
     try:
         statusInfo = json.loads(requests.get(url,timeout=5).content)
-
         devList = statusInfo['dev']
 
-        print '[CPU]: '+ str(statusInfo['cpu']['core']) + '核   '+ statusInfo['cpu']['hz'] + '   系统负载 '+ str(statusInfo['cpu']['load'])
-        print '[MAC]: '+ statusInfo['hardware']['mac']
-        print '[MEM]: Type: '+ statusInfo['mem']['type'] +'   Total: '+ statusInfo['mem']['total'] + '   Usage: ' +str(statusInfo['mem']['usage']*100)+'% \n'
+        print '[CPU]: ' + str(statusInfo['cpu']['core']) + '核   ' + statusInfo['cpu']['hz'] + '   系统负载 ' + str(statusInfo['cpu']['load'])
+        print '[MAC]: ' + statusInfo['hardware']['mac']
+        print '[MEM]: Type: ' + statusInfo['mem']['type'] + '   Total: ' + statusInfo['mem']['total'] + '   Usage: ' + str(statusInfo['mem']['usage']*100) + '% \n'
         print '--------------------[DEV]----------------------\n'
         for dev in devList:
-            print dev['mac']+ ' ' + dev['devname']
+            print dev['mac'] + ' ' + dev['devname']
     except Exception,e:
         print e
 
@@ -91,12 +89,12 @@ def getWanInfo(url):
     try:
         wanInfo = json.loads(requests.get(url,timeout=5).content)
         print '\n--------------------[WAN]----------------------\n'
-        print '用户: '+ wanInfo['info']['details']['username']
-        print '密码: '+ wanInfo['info']['details']['password']
-        print '类型: '+ wanInfo['info']['details']['wanType']
-        print 'IP地址: '+ wanInfo['info']['ipv4'][0]['ip']
-        print '网关: '+ wanInfo['info']['gateWay']
-        print 'DNS: '+ wanInfo['info']['dnsAddrs']+','+wanInfo['info']['dnsAddrs1']
+        print '用户: ' + wanInfo['info']['details']['username']
+        print '密码: ' + wanInfo['info']['details']['password']
+        print '类型: ' + wanInfo['info']['details']['wanType']
+        print 'IP地址: ' + wanInfo['info']['ipv4'][0]['ip']
+        print '网关: ' + wanInfo['info']['gateWay']
+        print 'DNS: ' + wanInfo['info']['dnsAddrs']+ ',' +wanInfo['info']['dnsAddrs1']
     except Exception,e:
         print e
 
@@ -105,7 +103,7 @@ def doReconnect(stop,start,pppoe):
     print '\n-----------------[Reconnect]-------------------\n'
     try:
         currentip = json.loads(requests.get(pppoe, timeout=5).content)['ip']['address']
-        print 'Current ip is '+ currentip
+        print 'Current ip is ' + currentip
         stopInfo = json.loads(requests.get(stop,timeout=5).content)
         if stopInfo['code'] == 0:
             print 'Sleep 3s...'
@@ -115,7 +113,7 @@ def doReconnect(stop,start,pppoe):
                 print 'Opration success... sleep 8s ...'
                 time.sleep(8)
                 newip = json.loads(requests.get(pppoe,timeout=5).content)['ip']['address']
-                print 'Success! New ip is '+newip
+                print 'Success! New ip is ' +newip
             else:
                 print 'Failed!'
         else:
@@ -126,7 +124,7 @@ def doReconnect(stop,start,pppoe):
 
 def doReboot(url):
     try:
-        reboot = json.loads(requests.get(url,timeout=5).content)
+        reboot = json.loads(requests.get(url, timeout=5).content)
 
         if reboot['code'] == 0:
             print 'Rebooting...'
@@ -144,6 +142,6 @@ if __name__ == '__main__':
     token = getToken(host)
 
     if token:
-        getInfo(host,token)
+        getInfo(host, token)
     else:
         print 'Login failed!'
